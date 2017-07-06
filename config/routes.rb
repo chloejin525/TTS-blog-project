b6d2c5328c2a7dfebe038c1f6c7454376c05f2f4
Ling Jin <chloejin525@gmail.com>
2017-07-05 20:47:22 -0400

    July-05 Blog Project first homework done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home' => 'welcome#home'
+    
   resources :comments
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'posts#index'
+  root 'welcome#home'
 end
